ca13b05a35ac967b6d91153720dcd588d90d7c15
Udayan2000 <ghosh.udayan2000@gmail.com>
2023-12-14 15:27:47 +0530

    up

--- src/Modal/Creator.jsx
+++ src/Modal/Creator.jsx
@@ -22,7 +22,7 @@ const Creator = ({ closeModal }) => {
   const [selectedSkills, setSelectedSkills] = useState([]);
   const [selectedSkillsName, setselectedSkillsName] = useState([]);
 
-  console.log(selectedSkills,"selectedskilssID", selectedSkillsName, "seletectedSKilss");
+  console.log(selectedSkills, "selectedskilssID", selectedSkillsName, "seletectedSKilss");
 
   const [user, setUser] = useState(initialState);
   const [showPassword, setShowPassword] = useState(false);
@@ -73,7 +73,7 @@ const Creator = ({ closeModal }) => {
   //selected Skilss
   const userSkilss = (id, skillname) => {
     // Add the skill ID to the array if it's not already present
-    if (selectedSkills.length<3) {
+    if (selectedSkills.length < 3) {
       if (
         !selectedSkills.includes(id) &&
         !selectedSkillsName.includes(skillname)
@@ -81,20 +81,20 @@ const Creator = ({ closeModal }) => {
         setSelectedSkills([...selectedSkills, id]);
         setselectedSkillsName([...selectedSkillsName, skillname]);
       }
-      
+
     }
-    else{
+    else {
       toast.error("U Can add only 3 skills")
     }
-  
-   
+
+
   };
   const handleChange = (e) => {
     const { name, value } = e.target;
     setUser({ ...user, [name]: value });
   };
 
-  const validation = async() => {
+  const validation = async () => {
     let error = {};
     if (user.firstname === "") {
       error.fullName = "first name Name required";
@@ -124,7 +124,7 @@ const Creator = ({ closeModal }) => {
       error.checkPassword = "password mismatched";
       await toast.error("checkpassword");
     }
-    if (selectedSkills.length===0) {
+    if (selectedSkills.length === 0) {
       error.selectedSkills = "check box";
       await toast.error("Select at least one skill");
     }
@@ -132,7 +132,7 @@ const Creator = ({ closeModal }) => {
       error.checked = "check box";
       await toast.error("tick the checkbox");
     }
-   
+
     return error;
   };
   const handleCountryCode = (code, countryEmoji, CountryId) => {
@@ -160,7 +160,7 @@ const Creator = ({ closeModal }) => {
       skillID: selectedSkills,
       // "skillID": ["63dcc310811c87e00cd3a676"]
     };
-   
+
 
     //  return console.log("DATAfgdgdfgdgdfd" , data);
 
@@ -182,178 +182,185 @@ const Creator = ({ closeModal }) => {
   return (
     <>
       <div className="influencermodal">
-        <div className="influencer_content">
-          <div className="icnacnt">
-            <div className="" onClick={() => closeModal()}>
-              <i className="fa-solid fa-arrow-left-long"></i>
+        <div className="smartphone">
+          <div className="influencer_content">
+            <div className="icnacnt">
+              <div className="" onClick={() => closeModal()}>
+                <i className="fa-solid fa-arrow-left-long"></i>
+              </div>
+              <p className="crttxtacnt">Create An Account</p>
             </div>
-            <p className="crttxtacnt">Create An Account</p>
-          </div>
-          <div className="crs" onClick={() => closeModal()}>
-            <i className="fa-solid fa-xmark"></i>
-          </div>
-          <div className="">
-            <form>
-              <div className="crttxtinpt">
-                <input
-                  type="text"
-                  name="firstname"
-                  value={user.firstname}
-                  placeholder="Your First Name"
-                  onChange={(e) => handleChange(e)}
-                />
-              </div>
-              <div className="crttxtinpt">
-                <input
-                  type="text"
-                  name="lastname"
-                  value={user.lastname}
-                  placeholder="Your Last Name"
-                  onChange={(e) => handleChange(e)}
-                />
-              </div>
-              <div className="crttxtinpt">
-                <input
-                  type="text"
-                  name="email"
-                  value={user.email}
-                  placeholder="Your Email"
-                  onChange={(e) => handleChange(e)}
-                />
-              </div>
-              <div className="txtinptphn">
-                <input
-                  type="number"
-                  name="phoneNumber"
-                  value={user.phoneNumber}
-                  placeholder=" Phone Number"
-                  onChange={(e) => handleChange(e)}
-                />
-                <div className="flgarwflx">
-                  <div className="numbflgarrow">
-                    {user.countryCode ? (
-                      user.emoji + "+" + user.countryCode
+            <div className="crs" onClick={() => closeModal()}>
+              <i className="fa-solid fa-xmark"></i>
+            </div>
+            <div className="">
+              <form>
+                <div className="crttxtinpt">
+                  <input
+                    type="text"
+                    name="firstname"
+                    value={user.firstname}
+                    placeholder="Your First Name"
+                    onChange={(e) => handleChange(e)}
+                  />
+                </div>
+                <div className="crttxtinpt">
+                  <input
+                    type="text"
+                    name="lastname"
+                    value={user.lastname}
+                    placeholder="Your Last Name"
+                    onChange={(e) => handleChange(e)}
+                  />
+                </div>
+                <div className="crttxtinpt">
+                  <input
+                    type="text"
+                    name="email"
+                    value={user.email}
+                    placeholder="Your Email"
+                    onChange={(e) => handleChange(e)}
+                  />
+                </div>
+                <div className="txtinptphn">
+                  <input
+                    type="number"
+                    name="phoneNumber"
+                    value={user.phoneNumber}
+                    placeholder=" Phone Number"
+                    onChange={(e) => handleChange(e)}
+                  />
+                  <div className="flgarwflx">
+                    <div className="numbflgarrow">
+                      {user.countryCode ? (
+                        user.emoji + "+" + user.countryCode
+                      ) : (
+                        <i className="fa-solid fa-flag"></i>
+                      )}
+                    </div>
+                    <div className="arrow" onClick={handleClicklist}>
+                      <i className="fa-solid fa-caret-down"></i>
+                    </div>
+                  </div>
+                  {country && (
+                    <div className="listcntry">
+                      <ul>
+                        {countryDetails?.map((item, index) => {
+                          // console.log(item)
+                          return (
+                            <>
+                              <li className="cntrynmb">
+                                <span className="">{item.emoji}</span>
+                                <span
+                                  className=""
+                                  key={index}
+                                  onClick={() =>
+                                    handleCountryCode(
+                                      item?.phone_code,
+                                      item?.emoji,
+                                      item?._id
+                                    )
+                                  }
+                                >
+                                  {item?.name}
+                                </span>
+                              </li>
+                            </>
+                          );
+                        })}
+                      </ul>
+                    </div>
+                  )}
+                </div>
+                <div className="crttxtinpt">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    name="password"
+                    placeholder="Password"
+                    value={user.password}
+                    onChange={(e) => handleChange(e)}
+                  />
+                  <div className="shwinpt" onClick={handleTogglePassword}>
+                    {showPassword ? (
+                      <i className="fa-solid fa-eye-slash"></i>
                     ) : (
-                      <i className="fa-solid fa-flag"></i>
+                      <i className="fa-regular fa-eye"></i>
                     )}
                   </div>
-                  <div className="arrow" onClick={handleClicklist}>
-                    <i className="fa-solid fa-caret-down"></i>
+                </div>
+                <div className="crttxtinpt">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    placeholder="ShowPassword"
+                    name="checkPassword"
+                    value={user.checkPassword}
+                    onChange={handleChange}
+                  />
+                  <div className="shwinpt" onClick={handleTogglePassword}>
+                    {showPassword ? (
+                      <i className="fa-solid fa-eye-slash"></i>
+                    ) : (
+                      <i className="fa-regular fa-eye"></i>
+                    )}
                   </div>
                 </div>
-                {country && (
-                  <div className="listcntry">
-                    <ul>
-                      {countryDetails?.map((item, index) => {
-                        // console.log(item)
-                        return (
-                          <>
-                            <li className="cntrynmb">
-                              <span className="">{item.emoji}</span>
-                              <span
-                                className=""
-                                key={index}
-                                onClick={() =>
-                                  handleCountryCode(
-                                    item?.phone_code,
-                                    item?.emoji,
-                                    item?._id
-                                  )
-                                }
-                              >
-                                {item?.name}
-                              </span>
-                            </li>
-                          </>
-                        );
-                      })}
-                    </ul>
+                <div className="sklinct">
+                  <i className="fa-solid fa-arrow-left-long"></i>
+                  <p className="crttxtacnt">Add Your Skill</p>
+                </div>
+                <div className="sklmax">
+                  <p className="skltxt">Maximum 3 skills</p>
+                  <div className="sprtdnccmd">
+                    {skills?.map((item, index) => {
+                      return (
+                        <>
+                          <div
+                            className="btnskl"
+                            key={index}
+                            onClick={() => {
+                              console.log(item, "userskills");
+                              userSkilss(item?._id, item?.skillName);
+                            }}
+                          >
+                            {item?.skillName}
+                          </div>
+                        </>
+                      );
+                    })}
                   </div>
-                )}
-              </div>
-              <div className="crttxtinpt">
-                <input
-                  type={showPassword ? "text" : "password"}
-                  name="password"
-                  placeholder="Password"
-                  value={user.password}
-                  onChange={(e) => handleChange(e)}
-                />
-                <div className="shwinpt" onClick={handleTogglePassword}>
-                  {showPassword ? (
-                    <i className="fa-solid fa-eye-slash"></i>
-                  ) : (
-                    <i className="fa-regular fa-eye"></i>
-                  )}
                 </div>
-              </div>
-              <div className="crttxtinpt">
-                <input
-                  type={showPassword ? "text" : "password"}
-                  placeholder="ShowPassword"
-                  name="checkPassword"
-                  value={user.checkPassword}
-                  onChange={handleChange}
-                />
-                <div className="shwinpt" onClick={handleTogglePassword}>
-                  {showPassword ? (
-                    <i className="fa-solid fa-eye-slash"></i>
-                  ) : (
-                    <i className="fa-regular fa-eye"></i>
-                  )}
-                </div>
-              </div>
-              <div className="sklinct">
-                <i className="fa-solid fa-arrow-left-long"></i>
-                <p className="crttxtacnt">Add Your Skill</p>
-              </div>
-              <div className="sklmax">
-                <p className="skltxt">Maximum 3 skills</p>
-                <div className="sprtdnccmd">
-                  {skills?.map((item, index) => {
-                    return (
-                      <>
-                        <div
-                          className="btnskl"
-                          key={index}
-                          onClick={() => {
-                            console.log(item, "userskills");
-                            userSkilss(item?._id, item?.skillName);
-                          }}
-                        >
-                          {item?.skillName}
-                        </div>
-                      </>
-                    );
-                  })}
-                </div>
-              </div>
 
-              <div className="addskl">
-                <p className="skltxt">View Skil</p>
-                <div className="sdmotohrflx">
-                  {selectedSkillsName?.map((item, index) => {
-                    return (
-                      <>
-                        <div className="btnskl">{item}</div>
-                      </>
-                    );
-                  })}
+                <div className="addskl">
+                  <p className="skltxt">View Skil</p>
+                  <div className="sdmotohrflx">
+                    {selectedSkillsName?.map((item, index) => {
+                      return (
+                        <>
+                          <div className="btnsklitm">
+                            {item}
+                            <div className="crsicn">
+                              <i className="fa-solid fa-xmark"></i>
+                            </div>
+                          </div>
+                        </>
+                      );
+                    })}
+                  </div>
                 </div>
-              </div>
 
-              <div className="chcktrm">
-                <input
-                  type="checkbox"
-                  checked={checked ? true : false}
-                  onClick={handleCheck}
-                />
-                <p className="trms">Terms & Condition</p>
-              </div>
-              <div className="sgnbtn" onClick={SignUp}>
-                Sign Up
-              </div>
-            </form>
+                <div className="chcktrm">
+                  <input
+                    type="checkbox"
+                    checked={checked ? true : false}
+                    onClick={handleCheck}
+                  />
+                  <p className="trms">Terms & Condition</p>
+                </div>
+                <div className="sgnbtn" onClick={SignUp}>
+                  Sign Up
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
--- src/View/Home/Index.jsx
+++ src/View/Home/Index.jsx
@@ -1,11 +1,89 @@
-import React, {  useState } from 'react'
+import React, { useState } from 'react'
 import homebanner from "../../Images/homebanner.jpg";
 import charublogo from "../../Images/charublogo.png";
 import kj from "../../Images/kj.jpg"
 import Influencer from '../../Modal/Influencer';
 import Scout from '../../Modal/Scout';
 import Creator from '../../Modal/Creator';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import dance from "../../Images/dance.jpg";
+import basketball from "../../Images/basketball.jpg";
+import boy from "../../Images/boy.jpg";
+import read from "../../Images/read.jpg";
+import femalephone from "../../Images/femalephone.png"
 const Index = () => {
+    var settings = {
+        // dots: true,
+        arrows:false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        initialSlide: 1,
+        fade: true,
+        autoplay: true,
+        speed: 3000,
+        cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
+        touchThreshold: 100,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    fade: true,
+                    autoplay: true,
+                    speed: 3000,
+                    cssEase: "ease-in",
+                    autoplaySpeed: 2000,
+                    // dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    initialSlide: 1,
+                    fade: true,
+                    autoplay: true,
+                    speed: 3000,
+                    cssEase: "ease-in",
+                    autoplaySpeed: 2000,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    fade: true,
+                    autoplay: true,
+                    speed: 3000,
+                    cssEase: "ease-in",
+                    autoplaySpeed: 2000,
+                }
+            }
+        ]
+    };
+    const slideimg =[
+        {
+             img:dance,
+        },
+        {
+            img:basketball,
+        },
+        {
+            img:boy,
+        },
+        {
+            img:read,
+        }
+    ]
 
     const [modala, setModala] = useState(false);
     const [modalb, setModalb] = useState(false);
@@ -22,11 +100,67 @@ const Index = () => {
     return (
         <>
             <section className='home_banner'>
-                <div className='ownimgdiv'>
+                {/* <div className='ownimgdiv'>
                     <img src={homebanner} className="" alt="..." />
+                </div> */}
+                <div className='cust_container'>
+                    <div className='row'>
+                        <div className='col-xl-4 col-lg-4 col-md-6  col-12'>
+                            <div className=''>
+                                <Slider {...settings}>
+                                    {slideimg.map((item, index) => {
+                                        return (
+                                            <div>
+                                               <div className='sldallimg' key={index}>
+                                                 <img src={item.img} alt="..." />
+                                               </div>
+                                            </div>
+                                        )
+                                    })}
+                                </Slider>
+                            </div>
+                        </div>
+                        <div className='col-xl-5 col-lg-5 col-md-6  col-12'>
+                            <div className=''>
+                                <div className='chrb'>
+                                    <img className="" src={charublogo} alt="..." />
+                                </div>
+                                <div className='tlntcrtapp'>
+                                    <p className='crttxt'>The Talent & Creators with Scout</p>
+                                    <p className='spttxt'>Super APP</p>
+                                </div>
+                                <div className=''>
+                                    <p className='alltxtspr'>The All-In-One Creators & Talent SUPERAPP !</p>
+                                    <p className='alltxtsprthn'>We are Unleashing Brilliance,
+                                        Connecting, Showcasing, and Elevating Talent and Creativity Globally.
+                                    </p>
+                                    <p className='alltxtsprbld'>By Invite only – Join us now by registering below!
+                                    </p>
+                                </div>
+                                <div className='btnmainbx'>
+                                    <div className='btbbx' onClick={handlemodalreg}>
+                                        Register as an Influencer
+                                    </div>
+                                    <div className='btbbx' onClick={handlemodalcre}>
+                                        Register as a Creator
+                                    </div>
+                                    <div className='btbbx' onClick={handlemodaltale}>
+                                        Register as a Growth Mentor  or Talent Scout
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-xl-3 col-lg-3 col-md-6  col-12'>
+                           <div className=''>
+                            <div className='phndiv'>
+                                <img src={femalephone} alt="..." />
+                            </div>
+                           </div>
+                        </div>
+                    </div>
                 </div>
             </section>
-            <section className='charubsuper'>
+            {/* <section className='charubsuper'>
                 <div className='cust_container'>
                     <div className='btnimgflx'>
                         <div className='chrblogo'>
@@ -62,7 +196,8 @@ const Index = () => {
                     </div>
 
                 </div>
-            </section>
+            </section> */}
+           
 
             {modala && <Influencer closeModal={setModala} />}
             {modalb && <Scout closeModal={setModalb} />}
